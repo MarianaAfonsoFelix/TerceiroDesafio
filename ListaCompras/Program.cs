f63d30151e2b2fdcab4eab0a782f27c7e69db6ec
Mari Afonso <marianaafonso007@gmail.com>
2024-01-04 20:20:57 -0300

    Criando a primeira versão do desafio

---
+++ ListaCompras/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Bem vinda(o), a sua Lista de comprar");
+Console.WriteLine("Por gentileza, digite seu nome:");
+var nome = Console.ReadLine();
+
+
+Console.WriteLine("Seja bem vindo a sua Lista de compras");
+Console.WriteLine(nome);
+
+Console.WriteLine("Vamos começar a adicionar itens a sua lista:");
+
+List<string> listaCompra = new List<string>();
+
+var continuidadeProcesso = 0;
+Console.WriteLine("Digite o item a ser adicionado, ao finalizar digite N para encerrar:");
+
+while(continuidadeProcesso == 0)
+{
+    
+    var produto = "N";
+    produto = Console.ReadLine();
+
+    if(produto == "N")
+        continuidadeProcesso = 1;
+    else
+        listaCompra.Add(produto);
+
+}
+
+Console.WriteLine(nome);
+Console.WriteLine("Não esqueça de passar no mercado e comprar:");
+foreach(var compra in listaCompra)
+{
+    Console.WriteLine(compra);
+}
+
